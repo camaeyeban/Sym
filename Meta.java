7faa2ef95257b2115d7822803212f56921ff3e6f
Peter Rupa <megatricycle@gmail.com>
2017-04-06 15:40:03 +0800

    Added more reserved functions

--- Meta.java
+++ Meta.java
@@ -10,7 +10,22 @@ public class Meta {
         "~",
         "++",
         "--",
-        "<-"
+        "<-",
+
+        "+",
+        "-",
+        "/",
+        "*",
+        "%",
+        "=",
+        "==",
+        "<",
+        "<=",
+        ">",
+        "=>",
+        "!=",
+        "&&",
+        "||"
     };
 
     public static char[] DELIMITERS = {
@@ -30,11 +45,25 @@ public class Meta {
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put("@", "FUNCTION_DECLARATION");
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put("->", "READ_INPUT");
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put(":", "PRINT");
-        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("#", "ASSIGNMENT_OPERATOR");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("#", "DECLARATION");
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put("?", "IF");
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put("~", "WHILE");
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put("++", "INCREMENT");
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put("--", "DECREMENT");
         RESERVED_FUNCTIONS_LOOKUP_TABLE.put("<-", "RETURN");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("+", "ADD");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("-", "SUBTRACT");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("*", "MULTIPLY");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("/", "DIVIDE");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("%", "MODULO");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("=", "ASSIGNMENT");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("==", "EQUALS");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("<", "LESS THAN");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("<=", "LESS THAN OR EQUAL TO");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put(">", "GREATER THAN");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put(">=", "GREATER THAN OR EQUAL TO");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("!=", "NOT EQUAL TO");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("&&", "AND");
+        RESERVED_FUNCTIONS_LOOKUP_TABLE.put("||", "OR");
     }
 }
